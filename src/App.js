5daf143e982d2c9a1ac2ca4e9dfc8f949b8de41a
Rudra <r82941037@gmail.com>
2021-02-19 23:40:28 +0530

    work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow'
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
+
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState('AED')
+  const [toCurrency, setToCurrency] = useState('AED')
+  const [exchangeRate, setExchangeRate] = useState(1.0)
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  const [paymentHistory, setPaymentHistory] = useState([])
+
+
+  let toAmount=0, fromAmount=0;
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
+
+  useEffect(() => {
+    fetch('https://openexchangerates.org/api/currencies.json')
+      .then(res => res.json())
+      .then(data => {
+        const firstCurrency = Object.keys(data)[0]
+        setCurrencyOptions([...Object.keys(data)])
+        setFromCurrency('AED')
+        setToCurrency(firstCurrency)
+        setExchangeRate(1.0)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`http://localhost:8080/convert/${fromCurrency}/${toCurrency}/`)
+        .then(res => res.json())
+        .then(data => {setExchangeRate(parseFloat(data))})
+    }
+  }, [fromCurrency, toCurrency])
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setPaymentHistory((oldPayments)=>[...oldPayments,<br/>,`${fromAmount} ${fromCurrency} -> ${toAmount} ${toCurrency}`]);
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setPaymentHistory((oldPayments)=>[`${toAmount} ${toCurrency} -> ${fromAmount} ${fromCurrency}`,<br/>,...oldPayments]);
+    setAmountInFromCurrency(false)
+  }
+
+  const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 5
+        }}
+    />
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
+      <div className="equals">=</div>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={e => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
+      <br/>
+      <h2>Payment History</h2>
+      <ColoredLine color="black"/>
+      <h2>From-------->To</h2>
+      <p>{paymentHistory}</p>
+    </>
   );
 }
 
